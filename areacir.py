a22b19e83c44f19796d0b459bd8bcb3bbad259ed
rishitavarshney12 <67853364+rishitavarshney12@users.noreply.github.com>
2020-10-22 07:28:37 +0200

    Add files via upload

---
+++ areacir.py
@@ -0,0 +1,5 @@
+# area of circle
+r=23
+print("area of circle:")
+area=3.14*r**2
+print(area)
---
+++ dict.py
@@ -0,0 +1,6 @@
+list={"Abc.py":"Python",
+      "Circum.c":"C",
+      "Sqroot.C++":"C++"}
+print("Input the File name:Abc.py" )
+print("The extension of the file is :")
+print(list.get("Abc.py"))
